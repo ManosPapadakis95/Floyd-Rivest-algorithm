5f260885495ae29c84f1c373cae1eac62ff33760
Manos Papadakis <papadakm95@gmail.com>
2021-03-08 00:15:28 +0200

    Update floyd-rivest.h

--- floyd-rivest.h
+++ floyd-rivest.h
@@ -8,6 +8,9 @@
 #define min(x,y) ((x<y)?x:y)
 #define swap(x,y) ((tmp=x),(x=y),(y=tmp))
 
+typedef int(*Func_int)(int,int);
+typedef int(*Func_double)(double,double);
+
 void ipartition(int array[],int left,int right,int k,Func_int cmp);
 void dpartition(double array[],int left,int right,int k,Func_double cmp);
 int inth(int array[],int k,int left,int right, Func_int cmp);
