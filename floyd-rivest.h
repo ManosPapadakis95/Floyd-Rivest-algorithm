263baf9a3502e1fa3701f9ec79d19aa2ec89ab55
Manos Papadakis <papadakm95@gmail.com>
2021-03-08 00:14:04 +0200

    Create floyd-rivest.h

---
+++ floyd-rivest.h
@@ -0,0 +1,16 @@
+
+#ifndef FLOYD_RIVEST_H
+#define FLOYD_RIVEST_H
+
+#define sgn(val) ((0 < val) - (val < 0))
+#define defaultCompare(a,b) ((a < b) ? -1 : (a > b ? 1 : 0))
+#define max(x,y) ((x<y)?y:x)
+#define min(x,y) ((x<y)?x:y)
+#define swap(x,y) ((tmp=x),(x=y),(y=tmp))
+
+void ipartition(int array[],int left,int right,int k,Func_int cmp);
+void dpartition(double array[],int left,int right,int k,Func_double cmp);
+int inth(int array[],int k,int left,int right, Func_int cmp);
+double dnth(double array[],int k,int left,int right, Func_double cmp);
+  
+#endif
