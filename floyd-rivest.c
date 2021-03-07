4392e76c9ef6cedbc70df9d45a8938d87e32fa43
Manos Papadakis <papadakm95@gmail.com>
2021-03-08 00:28:45 +0200

    Update floyd-rivest.c

--- floyd-rivest.c
+++ floyd-rivest.c
@@ -3,8 +3,8 @@
 
 // left is the left index for the interval
 // right is the right index for the interval
-// k is the desired index value, where array[k] is the (k+1)th smallest element when left = 0
-void ipartition(int array[],int left,int right,int k,Func_int cmp){
+// k is the desired index value, where obj[k] is the (k+1)th smallest element when left = 0
+void ipartition(int obj[],int left,int right,int k,Func_int cmp){
   int tmp;
   while (right > left) {
     // Use select recursively to sample a smaller set of size s
@@ -18,28 +18,28 @@ void ipartition(int array[],int left,int right,int k,Func_int cmp){
       double sd = 0.5 * sqrt(z * s * (n - s)/n) * sgn(i - n/2);
       int newLeft = max(left, int(k - i * s/n + sd));
       int newRight = min(right, int(k + (n - i) * s/n + sd));
-      ipartition(array, newLeft, newRight, k,cmp);
+      ipartition(obj, newLeft, newRight, k,cmp);
     }
     // partition the elements between left and right around t
-    int t = array[k] ;
+    int t = obj[k] ;
     int i = left;
     int j = right;
-    swap(array[left] , array[k]);
-    if (array[right] > t) {
-      swap (array[right] , array[left]);
+    swap(obj[left] , obj[k]);
+    if (obj[right] > t) {
+      swap (obj[right] , obj[left]);
     }
     while (i < j) {
-      swap (array[i] , array[j]);
+      swap (obj[i] , obj[j]);
       ++i;
       --j;
-      for (;cmp(array[i],t) < 0;++i);
-      for (;cmp(array[j],t) > 0;--j);
-      if (cmp(array[left] , t)==0) {
-        swap (array[left] , array[j]);
+      for (;cmp(obj[i],t) < 0;++i);
+      for (;cmp(obj[j],t) > 0;--j);
+      if (cmp(obj[left] , t)==0) {
+        swap (obj[left] , obj[j]);
       }
       else{
         ++j;
-        swap (array[j] , array[right]);
+        swap (obj[j] , obj[right]);
       }
       // Adjust left and right towards the boundaries of the subset
       // containing the (k - left + 1)th smallest element.
@@ -53,8 +53,8 @@ void ipartition(int array[],int left,int right,int k,Func_int cmp){
 
 // left is the left index for the interval
 // right is the right index for the interval
-// k is the desired index value, where array[k] is the (k+1)th smallest element when left = 0
-void dpartition(double array[],int left,int right,int k,Func_double cmp){
+// k is the desired index value, where obj[k] is the (k+1)th smallest element when left = 0
+void dpartition(double obj[],int left,int right,int k,Func_double cmp){
   double tmp;
   while (right > left) {
     // Use select recursively to sample a smaller set of size s
@@ -68,28 +68,28 @@ void dpartition(double array[],int left,int right,int k,Func_double cmp){
       double sd = 0.5 * sqrt(z * s * (n - s)/n) * sgn(i - n/2);
       int newLeft = max(left, int(k - i * s/n + sd));
       int newRight = min(right, int(k + (n - i) * s/n + sd));
-      dpartition(array, newLeft, newRight, k,cmp);
+      dpartition(obj, newLeft, newRight, k,cmp);
     }
     // partition the elements between left and right around t
-    double t = array[k] ;
+    double t = obj[k] ;
     int i = left;
     int j = right;
-    swap(array[left] , array[k]);
-    if (array[right] > t) {
-      swap (array[right] , array[left]);
+    swap(obj[left] , obj[k]);
+    if (obj[right] > t) {
+      swap (obj[right] , obj[left]);
     }
     while (i < j) {
-      swap (array[i] , array[j]);
+      swap (obj[i] , obj[j]);
       ++i;
       --j;
-      for (;cmp(array[i],t) < 0;++i);
-      for (;cmp(array[j],t) > 0;--j);
-      if (cmp(array[left] , t)==0) {
-        swap (array[left] , array[j]);
+      for (;cmp(obj[i],t) < 0;++i);
+      for (;cmp(obj[j],t) > 0;--j);
+      if (cmp(obj[left] , t)==0) {
+        swap (obj[left] , obj[j]);
       }
       else{
         ++j;
-        swap (array[j] , array[right]);
+        swap (obj[j] , obj[right]);
       }
       // Adjust left and right towards the boundaries of the subset
       // containing the (k - left + 1)th smallest element.
@@ -101,11 +101,11 @@ void dpartition(double array[],int left,int right,int k,Func_double cmp){
   }
 }
 
-int inth(int array[],int k,int left,int right, Func_int cmp){
-  ipartition(array,left,right,k,cmp);
-  return array[k-left+1];
+int inth(int obj[],int k,int left,int right, Func_int cmp){
+  ipartition(obj,left,right,k,cmp);
+  return obj[k-left+1];
 }
-double dnth(double array[],int k,int left,int right, Func_double cmp){
-  dpartition(array,left,right,k,cmp);
-  return array[k-left+1];
+double dnth(double obj[],int k,int left,int right, Func_double cmp){
+  dpartition(obj,left,right,k,cmp);
+  return obj[k-left+1];
 }
